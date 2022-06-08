23677349a837eee7efc0c5e7e39a4d1e0ff02312
Pavez <79183353+Pavez7274@users.noreply.github.com>
2022-06-07 21:14:12 -0300

    Delete src directory

--- src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-export const name = 'ready';
-export const type = 'dsc';
-export function run (): void {
-	const date = new Date(Date.now())
-		.toLocaleString('en-gb', {
-			timeZone: 'America/Mendoza'
-		})
-	console.log('* [client] :: Ready at ', date);
-};
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-// imports
-import { Lupo } from './structures/Lupo';
-
-// declarations
-const client = new Lupo();
-client.start();

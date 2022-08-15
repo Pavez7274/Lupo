b4325a79c545b0ee365efd07491946b4ad659e47
Pavez <79183353+Pavez7274@users.noreply.github.com>
2022-08-15 16:08:31 -0300

    Delete src directory

--- src/util/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './generateCommandDoc';
-export * from './resolveSnowflake';
-export * from './findMember'; 
-export * from './isBoolean';
-export * from './findUser';
-export * from './inspect';
-export * from './random';
